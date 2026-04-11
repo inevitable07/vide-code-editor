1fad74807d336544b7843420ddf2c1890f777e84
Aashish Bhaskar <imaashishbhaskar@gmail.com>
2026-04-11 16:21:32 +0530

    feat: implement Zustand-based file explorer with template initialization and file selection handling

--- app/playground/[id]/page.tsx
+++ app/playground/[id]/page.tsx
@@ -1,55 +1,90 @@
-"use client"
+"use client";
 
-import { Separator } from '@/components/ui/separator';
-import { SidebarInset, SidebarTrigger } from '@/components/ui/sidebar';
-import { TooltipProvider } from '@/components/ui/tooltip';
-import { TemplateFileTree } from '@/modules/playground/components/playground-explorer';
-import { usePlayground } from '@/modules/playground/hooks/usePlayground';
-import { useParams } from 'next/navigation'
-import React from 'react'
+import { Separator } from "@/components/ui/separator";
+import { SidebarInset, SidebarTrigger } from "@/components/ui/sidebar";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { TemplateFileTree } from "@/modules/playground/components/playground-explorer";
+import { useFileExplorer } from "@/modules/playground/hooks/useFileExplorer";
+import { usePlayground } from "@/modules/playground/hooks/usePlayground";
+import { TemplateFile } from "@/modules/playground/lib/path-to-json";
+import { useParams } from "next/navigation";
+import React, { useEffect } from "react";
 
 const MainPlayGround = () => {
-    const id = useParams<{id:string}>();
+  const id = useParams<{ id: string }>();
 
-    const {playgroundData, templateData, isLoading, error, saveTemplateData} = usePlayground(id?.id);
+  const { playgroundData, templateData, isLoading, error, saveTemplateData } =
+    usePlayground(id?.id);
 
-    console.log("playgroundData:", playgroundData);
-    console.log("templateData:", templateData);
-    const activeFile = "sample.txt";
+  const {
+    setTemplateData,
+    setActiveFileId,
+    setPlaygroundId,
+    setOpenFiles,
+    activeFileId,
+    closeAllFiles,
+    closeFile,
+    openFile,
+    openFiles,
+
+    // handleAddFile,
+    // handleAddFolder,
+    // handleDeleteFile,
+    // handleDeleteFolder,
+    // handleRenameFile,
+    // handleRenameFolder,
+    // updateFileContent
+  } = useFileExplorer();
+
+  useEffect(() => {
+    setPlaygroundId(id?.id || "");
+  }, [id, setPlaygroundId]);
+
+  useEffect(() => {
+    if (templateData && !openFiles.length) {
+      setTemplateData(templateData);
+    }
+  }, [templateData, setTemplateData, openFiles.length]);
+
+  const activeFile = openFiles.find((file)=>file.id === activeFileId);
+  const hasUnsavedChanges = openFiles.some((file) => file.hasUnsavedChanges);
+
+  const handleFileSelect = (file:TemplateFile) => {
+    openFile(file);
+  }
+
   return (
     <TooltipProvider>
       <>
-      <TemplateFileTree
-        data={templateData!}
-          onFileSelect={()=>{}}
+        <TemplateFileTree
+          data={templateData!}
+          onFileSelect={handleFileSelect}
           selectedFile={activeFile}
           title="File Explorer"
-          onAddFile={()=>{}}
-          onAddFolder={()=>{}}
-          onDeleteFile={()=>{}}
-          onDeleteFolder={()=>{}}
-          onRenameFile={()=>{}}
-          onRenameFolder={()=>{}}
-      />
-      <SidebarInset>
-        <header className='flex h-16 shrink-0 items-center gap-2 border-b px-4'>
-          <SidebarTrigger className='-ml-1'/>
-          <Separator orientation='vertical' className='mr-2 h-4' />
-        </header>
+          onAddFile={() => {}}
+          onAddFolder={() => {}}
+          onDeleteFile={() => {}}
+          onDeleteFolder={() => {}}
+          onRenameFile={() => {}}
+          onRenameFolder={() => {}}
+        />
+        <SidebarInset>
+          <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+            <SidebarTrigger className="-ml-1" />
+            <Separator orientation="vertical" className="mr-2 h-4" />
+          </header>
 
-        <div className='flex flex-1 items-center gap-2'>
-          <div className='flex flex-col flex-1'>
-            <h1 className='text-sm font-medium'>
-              {playgroundData?.title || "Code Playground"}
-            </h1>
-
+          <div className="flex flex-1 items-center gap-2">
+            <div className="flex flex-col flex-1">
+              <h1 className="text-sm font-medium">
+                {playgroundData?.title || "Code Playground"}
+              </h1>
+            </div>
           </div>
-
-        </div>
-      </SidebarInset>
+        </SidebarInset>
       </>
     </TooltipProvider>
-  )
-}
+  );
+};
 
-export default MainPlayGround
+export default MainPlayGround;
